573f6137848c168d08622697de86e507fb91c60e
hzliuj <hzliuj@tairanchina.com>
2017-09-14 16:40:18 +0800

    develop UserService

--- app/service/user/users.js
+++ app/service/user/users.js
@@ -7,6 +7,7 @@ module.exports = app => {
     // 发送注册验证码
     * sendSmsCode(mobile) {
       const res = yield this.ctx.curl(`${serverUrl}/validate/login/{mobile}`, {
+        method: 'POST',
         data: {
           phone: '"$mobile"'
         },
@@ -18,9 +19,56 @@ module.exports = app => {
     // 快速注册
     * quickRegister(mobile, smsCode){
       const res = yield this.ctx.curl(`${serverUrl}/auth/simple`, {
+        method: 'POST',
         data: {
           phone: '"$mobile"'
         },
+        dataType: 'json',
+
+        // 3 秒超时
+        timeout: 3000,
+      });
+      return res.data;
+    }
+
+    //注册
+    * register(mobile, smsCode， password){
+      const res = yield this.ctx.curl(`${serverUrl}/user/register`, {
+        method: 'POST',
+        data: {
+          phone: mobile,
+          smsCode: smsCode,
+          password: password
+        },
+        dataType: 'json',
+
+        // 3 秒超时
+        timeout: 3000,
+      });
+      return res.data;
+    }
+
+    //登录
+    * login(mobile, password){
+      const res = yield this.ctx.curl(`${serverUrl}/user/login`, {
+        method: 'POST',
+        data: {
+          phone: mobile,
+          password: password
+        },
+        dataType: 'json',
+
+        // 3 秒超时
+        timeout: 3000,
+      });
+      return res.data;
+    }
+
+    //登出
+    * logout(mobile, password){
+      const res = yield this.ctx.curl(`${serverUrl}/user/logout`, {
+        method: 'POST',
+        data: { },
         dataType: 'json',
 
         // 3 秒超时
